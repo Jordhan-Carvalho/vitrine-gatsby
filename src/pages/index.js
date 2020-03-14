692312f3a8205d75e57e2b9e0ab25930f433532e
Jordhan <jordhan.rdz@gmail.com>
2020-03-14 14:54:38 -0300

    Added google play link

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,17 @@ export default () => (
         Vitrine Virtual, jogá lá no Vitrine!
       </Typography.Title>
       <Typography.Title level={4} className='text-gray-600'>
-        Plataforma de compra e venda de produtos usados e novos, além de
-        catalogo de prestação de serviços na região oeste da bahia (Barreiras,
-        Luis Eduardo Magalhães)
+        Plataforma de compra e venda de produtos novos e usados, além de
+        catálogo de prestação de serviços na região oeste da bahia (Barreiras e
+        Luis Eduardo Magalhães).
       </Typography.Title>
-      <img src='/cover.png' alt='logo' className='mt-10' />
+      <a
+        href='https://play.google.com/store/apps/details?id=dev.jordhan.vitrine'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img src='/AndroidGooglePlay.png' alt='logo' className='mt-10' />
+      </a>
     </div>
   </LandingLayout>
 )
